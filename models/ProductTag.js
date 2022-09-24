512122754d05d1631a54552a0cf3336ae79a405b
Zachattack221 <zsvd09@gmail.com>
2022-09-24 13:11:14 -0400

    pulled starter code from updated module, installed npm dependencies

---
+++ models/ProductTag.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+  },
+
+  id: {
+    typeL DataTypes.INTEGER, 
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  tag_id: 
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
